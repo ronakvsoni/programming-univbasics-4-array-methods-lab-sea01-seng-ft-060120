39031822b5be0a6528c9fad3cc8f3592c730139f
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-25 12:15:19 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,31 @@
-def using_include(array, element)
+chocolates = ["snickers","Kit-Kat","Harsey's","Skittles","snickers"]
 
+def using_include(array, element)
+  array.include?(element)
 end
+p using_include(chocolates,"m&m")
+
 
 def using_sort(array)
-
+  array.sort
 end
+p using_sort(chocolates)
 
 def using_reverse(array)
+  array.reverse
 
 end
 
 def using_first(array)
+  array.first
 
 end
 
 def using_last(array)
-
+array.last
 end
 
 def using_size(array)
+  array.size
 
 end
---
+++ try.rb
@@ -0,0 +1,7 @@
+chocales_a = ["snickers","Kit-Kat","zarsey's"]
+chocolates = ["snickers","Kit-Kat","Harsey's","Skittles","snickers"]
+
+def using_include(array, element)
+  array.include?(element)
+end
+p using_include(chocolates,"m&m")
